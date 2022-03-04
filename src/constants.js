e7041bd72fdea27520498575e06654b19b779794
swipss <vassiljevsten1@gmail.com>
2022-03-04 13:16:46 +0200

    first commit

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+export const colors = {
+  black: "#121214",
+  darkgrey: "#3a3a3d",
+  grey: "#818384",
+  lightgrey: "#d7dadc",
+  primary: "#538d4e",
+  secondary: "#b59f3b",
+};
+
+export const colorsToEmoji = {
+  [colors.darkgrey]: "⬛",
+  [colors.primary]: "🟩",
+  [colors.secondary]: "🟧",
+};
+
+export const ENTER = "ENTER";
+export const CLEAR = "CLEAR";
+
+export const keys = [
+  ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+  ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+  [ENTER, "z", "x", "c", "v", "b", "n", "m", CLEAR],
+];
